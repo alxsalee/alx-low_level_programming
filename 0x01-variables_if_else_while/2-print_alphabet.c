4c6d7203900e5225bc5ef954cca03873c2ca0e27
alxsalee <fateemahali304@gmail.com>
2022-04-08 11:35:13 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.
        You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
        All your code should be in the main function
        You can only use putchar twice in your code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
- * main - program that prints the numbers from 00 to 99
- * Numbers must be separated by ,followed by a space
- * Numbers should be printed in ascending order with 2 digits
- * You can only use the putchar function
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
  * Return: 0
  */
 
 int main(void)
 {
-int n1 = 48;
-int n2 = 48;
-int com = 44;
-while (n1 <= 57)
+int l = 'a';
+while (l <= 'z')
 {
-n2 = 48;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 57 || n2 != 57)
-{
-putchar(com);
-putchar(32);
-}
-n2 += 1;
-}
-n1 += 1;
+putchar(l);
+l += 1;
 }
 putchar('\n');
 return (0);
