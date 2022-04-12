8af84e8b0b5c66a0c572edd59c3fb60e9fcac4ed
alxsalee <fateemahali304@gmail.com>
2022-04-12 04:25:45 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "0-putchar.c"
+int main(void)
+{
+	int n = 0;
+	char str_hbt[9] = "Holberton";
+
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
